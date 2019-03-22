0f6524727fcaa19de6b1984f4bc2422be1512bf9
Murray GM <murray.gm@gmail.com>
2019-03-22 12:19:38 -0700

    added a start date limiter
    If you've previously imported an IG archive and have a fresh down load you can now set a date to import from

--- prep_igdownload_files_for_macphotos.py
+++ prep_igdownload_files_for_macphotos.py
@@ -25,7 +25,20 @@ pathtoarchive='' #filepath to where your unzipped Instagram archive download is
 
 destFolder = '' #filepath to where you want to save renamed ALL items to
 
-igArchiveparts=['', '', ''] #array of each of the folders in the archive - DO NOT INCLUDE part 1 as no images eg 'murraygm_20190316_part_2/'
+igArchiveparts=['', '', ''] #array of each of the folders in the archive - DO NOT INCLUDE part 1 as no images
+
+
+#already imported your library and just want to add the new stuff from the full down load? 
+igStartDate = '' #add a starting date string formatted like so '2019-03-19 13:45:59' so that only items after this will be prepped
+
+try:
+	datetime.strptime(igStartDate, '%Y-%m-%d %H:%M:%S')
+except:
+	igStartDateFlag = 0
+	igStartDateStamp = datetime.timestamp(datetime.strptime('2010-07-15 00:00:00', '%Y-%m-%d %H:%M:%S'))
+else:	
+	igStartDateFlag = 1
+	igStartDateStamp = datetime.timestamp(datetime.strptime(igStartDate, '%Y-%m-%d %H:%M:%S'))
 
 
 for a in range(len(igArchiveparts)):
@@ -43,77 +56,78 @@ for a in range(len(igArchiveparts)):
 		for photos in data['photos']:
 			#pull creation date and make new string
 			fileDate = str.split( photos['taken_at'], 'T')
-			fileNameBit = str.split( photos['path'], '/')
-			newFname = fileDate[0]+'_'+fileNameBit[2]
-
-			origPath = targFolder+photos['path']
-			newPath = destFolder+newFname
-
-			nTitle=photos['caption']
-			nCreator='' #name of creator
-			nLocal=''
-
-			
-			if 'location' in photos:
-				nLocal=photos['location']
-
-			nCaption=photos['caption']
-
-			nKeywords= []
-			if "#" in nCaption:  
-				nKeywordsA=nCaption.split(' ')
-				nKeywordsB=[]
-
-				for i in range(len(nKeywordsA)):
-					nKeywordsB=nKeywordsA[i].split('#')
-					if len(nKeywordsB)>1:
-						nKeywords.append(nKeywordsB[1])
-
-			info = IPTCInfo(origPath)
-
-			info['object name'] = nTitle[:30]
-			info['content location name'] = nLocal
-			info['reference date'] = fileDate[0]
-			info['keywords'] = nKeywords
-			info['by-line'] = nCreator
-			info['caption/abstract'] = nCaption
-
-#			print(nTitle[:30], newFname)
-			info.save()
-			info.save_as(newPath)
 
 			#set the date on the file (last modified)
 			newDate = fileDate[0] + ' ' + fileDate[1]
 			newStamp = datetime.strptime(newDate, '%Y-%m-%d %H:%M:%S')
-
 			newCreateDate = datetime.timestamp(newStamp)
 
-			#set the new creation date
-			os.utime(newPath, (newCreateDate, newCreateDate))
+			if igStartDateStamp < newCreateDate:
+
+				fileNameBit = str.split( photos['path'], '/')
+				newFname = fileDate[0]+'_'+fileNameBit[2]
+
+				origPath = targFolder+photos['path']
+				newPath = destFolder+newFname
+
+				nTitle=photos['caption']
+				nCreator='' #added a creator name
+				nLocal=''
+
+				if 'location' in photos:
+					nLocal=photos['location']
+
+				nCaption=photos['caption']
+
+				nKeywords= []
+				if "#" in nCaption:  
+					nKeywordsA=nCaption.split(' ')
+					nKeywordsB=[]
+
+					for i in range(len(nKeywordsA)):
+						nKeywordsB=nKeywordsA[i].split('#')
+						if len(nKeywordsB)>1:
+							nKeywords.append(nKeywordsB[1])
+
+				info = IPTCInfo(origPath)
+
+				info['object name'] = nTitle[:30]
+				info['content location name'] = nLocal
+				info['reference date'] = fileDate[0]
+				info['keywords'] = nKeywords
+				info['by-line'] = nCreator
+				info['caption/abstract'] = nCaption
+
+	#			print(nTitle[:30], newFname)
+				info.save()
+				info.save_as(newPath)
+
+				#set the new creation date
+				os.utime(newPath, (newCreateDate, newCreateDate))
 
 		#Video
 		#loop through items
 		for videos in data['videos']:
 
 			vfileDate = str.split( videos['taken_at'], 'T')
-			vfileNameBit = str.split( videos['path'], '/')
-			vnewFname = vfileDate[0]+'_'+vfileNameBit[2]
-
-			vorigPath = targFolder+videos['path']
-			vnewPath = destFolder+vnewFname
-
-#			print (vorigPath)
-#			print (vnewPath)
-
-			#rename file
-			os.rename(vorigPath, vnewPath)
-
 			#set the date on the file (last modified)
 			vnewDate = vfileDate[0] + ' ' + vfileDate[1]
 			vnewStamp = datetime.strptime(vnewDate, '%Y-%m-%d %H:%M:%S')
-
 			vnewCreateDate = datetime.timestamp(vnewStamp)
 
-			#set the new creation date
-			os.utime(vnewPath, (vnewCreateDate, vnewCreateDate))
+			if igStartDateStamp < vnewCreateDate:
 
+				vfileNameBit = str.split( videos['path'], '/')
+				vnewFname = vfileDate[0]+'_'+vfileNameBit[2]
+
+				vorigPath = targFolder+videos['path']
+				vnewPath = destFolder+vnewFname
+
+	#			print (vorigPath)
+	#			print (vnewPath)
+
+				#rename file
+				os.rename(vorigPath, vnewPath)
+
+				#set the new creation date
+				os.utime(vnewPath, (vnewCreateDate, vnewCreateDate))
